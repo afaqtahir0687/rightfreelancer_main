8de798ac012ad9864d85c7922f0c36b92604f5c5
Murtaza395 <mughalmurtaza999@gmail.com>
2025-07-21 14:56:15 +0500

    small changes

--- core/Modules/Integrations/Resources/views/layouts/master.blade.php
+++ core/Modules/Integrations/Resources/views/layouts/master.blade.php
@@ -15,5 +15,17 @@
 
         {{-- Laravel Mix - JS File --}}
         {{-- <script src="{{ mix('js/pluginmanage.js') }}"></script> --}}
+
+        {{-- Google Analytics GT4 --}}
+        @if(get_static_option('google_analytics_gt4_status') == 'on' && get_static_option('google_analytics_gt4_ID'))
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id={{ get_static_option('google_analytics_gt4_ID') }}"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '{{ get_static_option('google_analytics_gt4_ID') }}');
+            </script>
+        @endif
     </body>
 </html>
